90f8d9489ef27a034a5ac45e21321f4e8e8950f8
unknown <Alian59@Alian59.alian.aliansoftware.org>
2022-02-07 11:32:56 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <login/>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import login from './components/core/LoginUser.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    login
   }
 }
 </script>
@@ -21,6 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/use/Navbar.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="container">
+    <ul class="list">
+        <li class="con_item"><a href=" #">Home</a></li>
+        <li class="con_item"><a href="#">About</a></li>
+        <li class="con_item"><a href="#">Signup</a></li>
+        <li class="con_item"><a href="#">Login</a></li>
+    <input class="serch_elm" type="serch" />
+    <button class="serch_btn">Serch</button>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Nav"
+}
+</script>
+
+<style scoped>
+html{
+    margin: 0px;
+    padding: 0px;
+}
+.container{
+    margin: 0px;
+    padding: 0px;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+.serch_elm{
+    padding-top: 5px;
+    margin-top: 11px;
+    margin-left: 300px;
+    border-radius: 5px;
+}
+.serch_btn{
+    margin-left: 5px;
+    padding-bottom: -5px;
+    padding-top: 2px;
+    border-radius: 5px;
+    background-color: blanchedalmond;
+    color: black;
+}
+.serch_btn:hover{
+    background-color: #333;
+    color: whitesmoke;
+}
+</style>
